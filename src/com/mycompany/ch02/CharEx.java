783a9cf0b64f2e1cd01ecfe3cf987a45f96bdfb9
bluesky7660 <chlee7660@gmail.com>
2024-07-26 12:35:16 +0900

    ch02 2

---
+++ src/com/mycompany/ch02/CharEx.java
@@ -0,0 +1,20 @@
+package com.mycompany.ch02;
+
+public class CharEx {
+
+	public static void main(String[] args) {
+		char var = 'A';
+//		char var2 = "A";
+//		char var3 = 'ssakoqnouqv';
+		char var1 = 'A';
+		//10진수의 문자
+		char var2 = 65;
+		//16진수 문자
+		char var3 = '\u0041';
+		
+		System.out.println("var1: " + var1);
+		System.out.println("var2: " + var2);
+		System.out.println("var3: " + var3);
+	}
+
+}
---
+++ src/com/mycompany/ch02/FloatDoubleEx.java
@@ -0,0 +1,19 @@
+package com.mycompany.ch02;
+
+public class FloatDoubleEx {
+
+	public static void main(String[] args) {
+		float var1 = 3.14f;
+		double var2 = 3.14;
+		
+		//정밀도
+		float var3 = 0.123456789123456789f;
+		double var4 = 0.123456789123456789;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
