21d393140bc9145a346661e6b84ca69c4dabb6b9
bluesky7660 <chlee7660@gmail.com>
2024-07-29 10:11:18 +0900

    constants

---
+++ src/com/mycompany/ch02/ConstantsEx.java
@@ -0,0 +1,11 @@
+package com.mycompany.ch02;
+
+public class ConstantsEx {
+	public static final double PAI = 3.14;	//상수선언
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//PAI = 1.23; //상수선언을 해서 재할당을 못함
+	}
+
+}
