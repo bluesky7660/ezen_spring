97aaaaf4a635f284284f4be9dcce82d9d8bfe419
bluesky7660 <chlee7660@gmail.com>
2024-08-13 17:28:04 +0900

    0813 1

---
+++ src/com/mycompany/ch10/first/TryCatchFinallyEx.java
@@ -0,0 +1,16 @@
+package com.mycompany.ch10.first;
+
+public class TryCatchFinallyEx {
+
+	public static void main(String[] args) {
+		
+		try {
+			Class clazz = Class.forName("hjvj");
+			System.out.println("ajdniuqbuf");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/com/mycompany/ch10/second/ThrowsEx.java
@@ -0,0 +1,17 @@
+package com.mycompany.ch10.second;
+
+public class ThrowsEx {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	public static void findClass() throws ClassNotFoundException {
+		Class clazz = Class.forName("hjvj");
+	}
+
+}
